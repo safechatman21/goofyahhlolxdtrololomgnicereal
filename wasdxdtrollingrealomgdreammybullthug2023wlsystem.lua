f255a0b2c36417347e50f540a7ff2a580533116a
safechatman21 <139033789+safechatman21@users.noreply.github.com>
2023-08-06 20:33:42 -0400

    Create wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua

---
+++ wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua
@@ -0,0 +1,20 @@
+local VIPWhitelist = {
+    -- Add the user IDs of VIP 
+}
+
+local MVPWhitelist = {
+    195179681;
+    1419654389;
+    2729773482;
+    4017627909;
+    1234062644;
+    770787680;
+    704272740;
+    3659417278;
+    2319188892;
+    104770575;
+}
+
+local EliteWhitelist = {
+    4632729483;
+}
