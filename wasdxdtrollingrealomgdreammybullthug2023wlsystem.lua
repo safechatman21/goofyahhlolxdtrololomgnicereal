cf55b2d694069bac84761217aca3764f29ed4b05
safechatman21 <139033789+safechatman21@users.noreply.github.com>
2023-08-20 02:38:41 -0400

    Update wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua

--- wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua
+++ wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua
@@ -6,7 +6,7 @@ MVPWhitelist = {
     280504764; -- 7figts
     608053304; -- buyer UPTYHM
     2729773482; --yk_ts
-    4017627909; --slitted WHO TF ARE THESE PPL CAM
+    4017627909; --slitted WHO TF ARE THESE PPL CAM -- bro idfk LOL
     1234062644; --v7u0
     770787680; --moneylaundered
     704272740; --soph4k2
