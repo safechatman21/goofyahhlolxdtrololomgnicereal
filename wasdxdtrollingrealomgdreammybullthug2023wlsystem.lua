9abb93e4b7b0d494dc076fed487a7a8e8342a71f
y333d <104227198+y333d@users.noreply.github.com>
2023-09-01 07:38:22 +0200

    Update wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua

--- wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua
+++ wasdxdtrollingrealomgdreammybullthug2023wlsystem.lua
@@ -17,6 +17,7 @@ MVPWhitelist = {
     770787680; --moneylaundered
     704272740; --soph4k2
     3659417278; --justgotvoice
+    346025878;
     2319188892; --makepluh
     3143633725; -- big booster
     1565568713;  --free whitelist by YMXM
